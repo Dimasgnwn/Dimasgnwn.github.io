0a2605c99d60d44ccfeddd4630a6e7adc1546f46
Dimasgnwn <gunawandimas82@gmail.com>
2024-08-26 21:31:07 +0700

    update font size

--- js/script.js
+++ js/script.js
@@ -19,3 +19,29 @@ window.onscroll = () => {
 
     header.classList.toggle('sticky',window.scrollY > 100);
 };
+
+// const menuIcon = document.getElementById('menu-icon');
+// const navbar = document.querySelector('.navbar');
+
+// function toggleMenu() {
+//   if (window.innerWidth <= 991) {
+//     // Jika lebar layar kurang dari atau sama dengan 991px
+//     menuIcon.textContent = ''; // Kosongkan isi ikon menu
+//     navbar.querySelectorAll('a').forEach(link => {
+//       menuIcon.appendChild(link); // Tambahkan setiap link ke dalam ikon menu
+//       menuIcon.classList.toggle('active');
+//     });
+//   } else {
+//     // Jika lebar layar lebih dari 991px
+//     navbar.appendChild(...menuIcon.childNodes); // Kembalikan semua link ke navbar
+//     menuIcon.textContent = 'bx bx-menu'; // Kembalikan ikon menu ke semula
+//   }
+// }
+
+// // Panggil fungsi saat halaman dimuat dan setiap kali ukuran jendela berubah
+// window.addEventListener('load', toggleMenu);
+// window.addEventListener('resize', toggleMenu);
+// menuIcon.addEventListener('click', toggleMenu);
+
+
+  
